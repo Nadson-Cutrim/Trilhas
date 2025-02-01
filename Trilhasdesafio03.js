318e503793fe75d2b3ab50556a199e0a0c7a5989
Nadson Rodrigues Cutrim Junior <134233605+Nadson-Cutrim@users.noreply.github.com>
2025-02-01 16:40:46 -0300

    Trilhasdesafio03.js

---
+++ Trilhasdesafio03.js
@@ -0,0 +1,4 @@
+//Crie uma variável chamada preco com o valor 50 e uma variável desconto com o valor 0.2 (20%). Calcule o preço com desconto e exiba o valor final.
+let preco = 50; // preço do produto
+let desconto = 0.2; //valor do desconto
+console.log(`Este é o seu desconto senhorª ${preco * desconto}`); //exibe o valor do desconto no console
