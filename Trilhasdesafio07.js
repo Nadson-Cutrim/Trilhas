2130c9f888a77c5a4eee47b70f2e8b4672077c5a
Nadson Rodrigues Cutrim Junior <134233605+Nadson-Cutrim@users.noreply.github.com>
2025-02-01 16:55:19 -0300

    Trilhasdesafio07.js

---
+++ Trilhasdesafio07.js
@@ -0,0 +1,4 @@
+//Crie duas variáveis, numero1 e numero2, e atribua valores a elas. Verifique se os dois números são iguais e, caso sejam, exiba "Os números são iguais". Caso contrário, exiba "Os números são diferentes".
+let num1 = 10; // Inicializa a variável num1 com o valor 
+let num2 = 10; // Inicializa a variável num2 com o valor 
+num1 == num2 ? console.log('Os números são iguais') : console.log('Os números são diferentes'); // Verifica se os números são iguais e imprime a mensagem correspondente
