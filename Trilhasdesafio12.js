e3347dcccb0d84bbaac14d61a529371a44e23b60
Nadson Rodrigues Cutrim Junior <134233605+Nadson-Cutrim@users.noreply.github.com>
2025-02-01 16:59:58 -0300

    Trilhasdesafio12.js

---
+++ Trilhasdesafio12.js
@@ -0,0 +1,7 @@
+//Crie um loop que exiba todos os números pares de 0 a 20.
+let i = 0; // Inicializa o contador
+// Loop while para exibir números pares de 0 a 20
+while (i <= 20) {
+    console.log(`Numeros pares entre zero e vinte : ${i}`); // Imprime o número par
+    i += 2; // Incrementa o contador em 2 para obter o próximo número par
+}
