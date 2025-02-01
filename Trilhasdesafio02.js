e5e06a1416dfd7b5e3c73126e013564add1dac56
Nadson Rodrigues Cutrim Junior <134233605+Nadson-Cutrim@users.noreply.github.com>
2025-02-01 16:51:10 -0300

    Trilhasdesafio02.js

---
+++ Trilhasdesafio02.js
@@ -0,0 +1,4 @@
+//Crie duas variáveis: uma chamada idade e outra chamada altura. Atribua a idade o valor 25 e a altura o valor 1.75. Exiba ambos os valores.
+let altura = 1.75; // altura em metros
+let idade = 25; // idade
+console.log(`A altura é ${altura} e a idade é ${idade} !`); // A altura é 1.75 e a idade é 25 !
