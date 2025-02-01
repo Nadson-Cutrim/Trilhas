06f104911e8246f1b92c4d2553ebb4ba39fbe479
Nadson Rodrigues Cutrim Junior <134233605+Nadson-Cutrim@users.noreply.github.com>
2025-02-01 16:59:01 -0300

    Trilhasdesafio11.js

---
+++ Trilhasdesafio11.js
@@ -0,0 +1,9 @@
+//Crie um loop que imprima a tabuada do número 7, de 1 a 10.
+const numero = 7; // constante: Número para o qual queremos a tabuada
+let i = 1; // Inicializa o contador
+
+// Loop while para imprimir a tabuada do número 7
+while (i <= 10) {
+    console.log(`Resultado da Multipicação: ${numero} x ${i} = ${numero * i}`); // Imprime a multiplicação
+    i++; // Incrementa o contador
+}
