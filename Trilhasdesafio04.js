0107eedbbe12989d08de509e91702f8ef12982f4
Nadson Rodrigues Cutrim Junior <134233605+Nadson-Cutrim@users.noreply.github.com>
2025-02-01 16:52:06 -0300

    Trilhasdesafio04.js

---
+++ Trilhasdesafio04.js
@@ -0,0 +1,3 @@
+//Crie uma variável chamada temperatura e atribua o valor 30. Se a temperatura for maior que 25, exiba a mensagem "Está calor!". Caso contrário, exiba "Está fresco!".
+let temperatura = 30; //altere a temperatura para testar o código
+temperatura > 25 ? console.log(`Está quente, vamos a praia`) : console.log(`Está frio, vamos ficar em casa`); //condição ternária, se a temperatura for maior que 25 imprime a primeira mensagem, se não imprime a segunda mensagem
