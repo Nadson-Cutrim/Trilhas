9a83bdb17b8fd98882864976228ddd23067d2d75
Nadson Rodrigues Cutrim Junior <134233605+Nadson-Cutrim@users.noreply.github.com>
2025-02-01 16:56:13 -0300

    Trilhasdesafio08.js

---
+++ Trilhasdesafio08.js
@@ -0,0 +1,4 @@
+//Crie uma variável chamada nome e uma variável chamada idade. Exiba a mensagem "Olá, meu nome é [nome] e eu tenho [idade] anos", utilizando concatenação.
+let nome = ('Nadson Cutrim Sou aluno do trilhas'); // Nome do aluno
+let idade = 24; // Idade do aluno
+console.log(`Olá, meu nome é ${nome}, eu tenho  ${idade} anos`); // Imprime o nome e a idade do aluno
