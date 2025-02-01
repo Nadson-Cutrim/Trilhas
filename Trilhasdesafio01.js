01c388b0d9bbc8be8a4ded4766ada64416fc2b73
Nadson Rodrigues Cutrim Junior <134233605+Nadson-Cutrim@users.noreply.github.com>
2025-02-01 17:06:55 -0300

    Trilhasdesafio01.js

---
+++ Trilhasdesafio01.js
@@ -0,0 +1,3 @@
+//Crie uma variável chamada nome e atribua seu nome a ela. Em seguida, exiba o valor dessa variável.
+let nome = ('NADSON'); // adiciona o nome
+alert(`Olá ${nome}`); //implemente a função que exibe o alerta
