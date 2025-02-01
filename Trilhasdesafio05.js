ecfb85d2ffbc813fa22768bd9bc0842dbbf31256
Nadson Rodrigues Cutrim Junior <134233605+Nadson-Cutrim@users.noreply.github.com>
2025-02-01 16:44:58 -0300

    Trilhasdesafio05.js

---
+++ Trilhasdesafio05.js
@@ -0,0 +1,3 @@
+//Crie uma variável idade e atribua um valor. Se a pessoa for maior de idade (18 ou mais), exiba "Você é maior de idade". Caso contrário, exiba "Você é menor de idade".
+let idade =  18; //altere a idade para testar o código
+(idade >= 18) ? console.log('Maior de Idade') : console.log('Menor de Idade'); //condição ternária, se a idade for maior ou igual a 18 imprime a primeira mensagem, se não imprime a segunda mensagem
