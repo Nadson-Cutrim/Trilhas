370e7a8bde111fad7ad6c8e3d1a61c06ad32fd40
Nadson Rodrigues Cutrim Junior <134233605+Nadson-Cutrim@users.noreply.github.com>
2025-02-01 17:00:48 -0300

    Trilhadesafio13.js

---
+++ Trilhadesafio13.js
@@ -0,0 +1,4 @@
+//Escreva um código que calcule a área de um círculo. Utilize uma função para realizar o cálculo. A função deve receber o raio como parâmetro e retornar a área.
+let raio = 10; // Raio do círculo
+let area = Math.PI * raio * raio; // Expressão de Calculo da área do círculo
+console.log(`A área do círculo é ${area.toFixed()}`); // resultado da área do círculo 
