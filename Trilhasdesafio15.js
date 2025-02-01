71b20d01ec628ea74d9e991e69dde375232b4eb1
Nadson Rodrigues Cutrim Junior <134233605+Nadson-Cutrim@users.noreply.github.com>
2025-02-01 17:05:48 -0300

    Trilhasdesafio15.js

---
+++ Trilhasdesafio15.js
@@ -0,0 +1,8 @@
+/*Refatore o código abaixo para que seja mais legível, usando boas práticas de nomenclatura e separando o código em funções:
+x = 10
+y = 20
+z = x+y
+console.log(z)*/
+
+let x = 2; y = 3; // Atribui valores as variáveis x e y
+console.log(`O resultado da soma entre: ${x} + ${y} é ${x + y} `); //Resultado da soma entre x e y
