e0c3464ee3653ef8f7bcfe1ab2e11b9619e297e0
Nadson Rodrigues Cutrim Junior <134233605+Nadson-Cutrim@users.noreply.github.com>
2025-02-01 17:01:28 -0300

    Trilhadesafio14.js

---
+++ Trilhadesafio14.js
@@ -0,0 +1,3 @@
+//Comente seu código explicando o que cada parte faz. Crie um programa simples que calcule a soma de dois números e imprima o resultado.
+let z = 2 + 2; // Inicializa a variável z com o valor 4
+console.log(`O valor de z é ${z}`); // Imprime o valor de z
