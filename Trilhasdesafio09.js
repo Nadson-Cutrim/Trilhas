0600cb1e5cfdca9411ed8a60ede1c77a5b7f5819
Nadson Rodrigues Cutrim Junior <134233605+Nadson-Cutrim@users.noreply.github.com>
2025-02-01 16:57:13 -0300

    Trilhasdesafio09.js

---
+++ Trilhasdesafio09.js
@@ -0,0 +1,7 @@
+//Crie um loop que imprima os números de 1 a 10 na tela.
+let contador = 1; // inicializa o contador
+// Loop while para exibir números de 1 a 10
+  while(contador <= 10  ){
+console.log(` Seu número  ${contador}`);// Imprime o número
+contador++;  // Incrementa o contador
+}
